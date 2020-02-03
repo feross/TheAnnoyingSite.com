bd6632865cb77794e61db680372005202f69156a
Jack <29169102+Jack5079@users.noreply.github.com>
2020-02-02 21:24:17 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -384,7 +384,24 @@ function startVibrateInterval () {
     const duration = Math.floor(Math.random() * 600)
     window.navigator.vibrate(duration)
   }, 1000)
+
+  // If the gamepad can vibrate, we will at random intervals every second. And at random strengths!
+  window.addEventListener('gamepadconnected', (event) => {
+    const gamepad = event.gamepad
+    if (gamepad.vibrationActuator) {
+      setInterval(() => {
+        if (gamepad.connected) {
+          gamepad.vibrationActuator.playEffect('dual-rumble', {
+            duration: Math.floor(Math.random() * 600),
+            strongMagnitude: Math.random(),
+            weakMagnitude: Math.random()
+          })
+        }
+      }, 1000)
+    }
+  })
 }
+
 
 /**
  * Intercept all user-initiated events and call the given the function, `onInput`.
