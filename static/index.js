988a6c2a5679bfe7fe5558a9d3deb581414281e2
Feross Aboukhadijeh <feross@feross.org>
2021-06-20 22:14:13 -0700

    add midi, bluetooth, usb, serial

--- static/index.js
+++ static/index.js
@@ -201,6 +201,7 @@ function init () {
     event.preventDefault()
     event.stopPropagation()
 
+    requestPointerLock()
     startVibrateInterval()
     enablePictureInPicture()
     triggerFileDownload()
@@ -208,15 +209,24 @@ function init () {
     focusWindows()
     copySpamToClipboard()
     speak()
-    requestPointerLock()
+    requestClipboardRead()
+    if (!window.ApplePaySession) {
+      // Don't request TouchID on every interaction in Safari since it blocks
+      // the event loop and stops windows from moving
+      requestWebauthnAttestation()
+    }
+    requestMidiAccess()
+    requestBluetoothAccess()
+    requestUsbAccess()
+    requestSerialAccess()
+    requestHidAccess()
+    requestCameraAndMic()
+    requestFullscreen()
 
     // Capture key presses on the Command or Control keys, to interfere with the
     // "Close Window" shortcut.
     if (event.key === 'Meta' || event.key === 'Control') {
       window.print()
-    } else {
-      if (isParentWindow && Math.random() < 0.20) requestFullscreen()
-      else requestCameraAndMic()
     }
 
     // 'touchstart' and 'touchend' events are not able to open a new window
@@ -468,6 +478,21 @@ function animateUrlWithEmojis () {
 }
 
 /**
+ * Lock the user's pointer, without even being in full screen!
+ * Require user-initiated event.
+ */
+function requestPointerLock () {
+  const requestPointerLockApi = (
+    document.body.requestPointerLock ||
+    document.body.webkitRequestPointerLock ||
+    document.body.mozRequestPointerLock ||
+    document.body.msRequestPointerLock
+  )
+
+  requestPointerLockApi.call(document.body)
+}
+
+/**
  * Start vibrating the device at random intervals, on supported devices.
  * Requires user-initiated event.
  */
@@ -590,18 +615,146 @@ function speak (phrase) {
 }
 
 /**
- * Lock the user's pointer, without even being in full screen!
- * Require user-initiated event.
+ * Attempt to read the user's clipboard.
+ * Requires user-initiated event.
  */
-function requestPointerLock () {
-  const requestPointerLockApi = (
-    document.body.requestPointerLock ||
-    document.body.webkitRequestPointerLock ||
-    document.body.mozRequestPointerLock ||
-    document.body.msRequestPointerLock
-  )
+function requestClipboardRead () {
+  try {
+    navigator.clipboard.readText().then(
+      data => {
+        if (!window.ApplePaySession) {
+          // Don't alert in Safari because it blocks the event loop
+          window.alert("Successfully read data from clipboard: '" + data + "'")
+        }
+      },
+      () => {}
+    )
+  } catch {}
+}
 
-  requestPointerLockApi.call(document.body)
+/**
+ * Request Webauthn attestation.
+ * Requires user-initiated event.
+ */
+function requestWebauthnAttestation () {
+  try {
+    // From https://developer.mozilla.org/en-US/docs/Web/API/Web_Authentication_API
+    // This code is public domain, per https://developer.mozilla.org/en-US/docs/MDN/About#Copyrights_and_licenses
+
+    // sample arguments for registration
+    const createCredentialDefaultArgs = {
+      publicKey: {
+      // Relying Party (a.k.a. - Service):
+        rp: {
+          name: 'Acme'
+        },
+
+        // User:
+        user: {
+          id: new Uint8Array(16),
+          name: 'john.p.smith@example.com',
+          displayName: 'John P. Smith'
+        },
+
+        pubKeyCredParams: [{
+          type: 'public-key',
+          alg: -7
+        }],
+
+        attestation: 'direct',
+
+        timeout: 60000,
+
+        challenge: new Uint8Array([ // must be a cryptographically random number sent from a server
+          0x8C, 0x0A, 0x26, 0xFF, 0x22, 0x91, 0xC1, 0xE9, 0xB9, 0x4E, 0x2E, 0x17, 0x1A, 0x98, 0x6A, 0x73,
+          0x71, 0x9D, 0x43, 0x48, 0xD5, 0xA7, 0x6A, 0x15, 0x7E, 0x38, 0x94, 0x52, 0x77, 0x97, 0x0F, 0xEF
+        ]).buffer
+      }
+    }
+
+    // sample arguments for login
+    const getCredentialDefaultArgs = {
+      publicKey: {
+        timeout: 60000,
+        // allowCredentials: [newCredential] // see below
+        challenge: new Uint8Array([ // must be a cryptographically random number sent from a server
+          0x79, 0x50, 0x68, 0x71, 0xDA, 0xEE, 0xEE, 0xB9, 0x94, 0xC3, 0xC2, 0x15, 0x67, 0x65, 0x26, 0x22,
+          0xE3, 0xF3, 0xAB, 0x3B, 0x78, 0x2E, 0xD5, 0x6F, 0x81, 0x26, 0xE2, 0xA6, 0x01, 0x7D, 0x74, 0x50
+        ]).buffer
+      }
+    }
+
+    // register / create a new credential
+    navigator.credentials.create(createCredentialDefaultArgs)
+      .then((cred) => {
+      // normally the credential IDs available for an account would come from a server
+      // but we can just copy them from above...
+        const idList = [{
+          id: cred.rawId,
+          transports: ['usb', 'nfc', 'ble'],
+          type: 'public-key'
+        }]
+        getCredentialDefaultArgs.publicKey.allowCredentials = idList
+        return navigator.credentials.get(getCredentialDefaultArgs)
+      })
+  } catch {}
+}
+
+/**
+ * Request access to MIDI devices.
+ * Requires user-initiated event.
+ */
+function requestMidiAccess () {
+  try {
+    navigator.requestMIDIAccess({
+      sysex: true
+    })
+  } catch {}
+}
+
+/**
+ * Request access to Bluetooth devices.
+ * Requires user-initiated event.
+ */
+function requestBluetoothAccess () {
+  try {
+    navigator.bluetooth.requestDevice({
+      // filters: [...] <- Prefer filters to save energy & show relevant devices.
+      // acceptAllDevices here ensures dialog can populate, we don't care with what.
+      acceptAllDevices: true
+    })
+      .then(device => device.gatt.connect())
+  } catch {}
+}
+
+/**
+ * Request access to USB devices.
+ * Requires user-initiated event.
+ */
+function requestUsbAccess () {
+  try {
+    navigator.usb.requestDevice({ filters: [{}] })
+  } catch {}
+}
+
+/**
+ * Request access to Serial devices.
+ * Requires user-initiated event.
+ */
+function requestSerialAccess () {
+  try {
+    navigator.serial.requestPort({ filters: [] })
+  } catch {}
+}
+
+/**
+ * Request access to HID devices.
+ * Requires user-initiated event.
+ */
+function requestHidAccess () {
+  try {
+    navigator.hid.requestDevice({ filters: [] })
+  } catch {}
 }
 
 /**
